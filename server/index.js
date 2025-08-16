6d4ba925db67a6c4c0baef505f4a7d11517c7d13
himavanth508 <himavanth508@gmail.com>
2025-08-16 20:33:58 +0530

    Initial commit of the project

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import userRouter from "./routes/userRoutes.js";
+import todoRouter from "./routes/todoRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
+import likeRouter from "./routes/likeRoutes.js";
+import mongoose from "mongoose";
+import path from "path"
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/images", express.static("images"));
+
+
+app.use("/users", userRouter);
+app.use("/todos", todoRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+app.use("/likes", likeRouter);
+
+// app.use("/products", postRouter);
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/socialdb1")
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
